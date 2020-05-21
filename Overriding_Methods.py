7c13bdf8432d9fdc3c34b7e96021ae9aadec2b41
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-21 20:06:28 +0700

    Super()

--- Overriding_Methods.py
+++ Overriding_Methods.py
@@ -1,3 +1,4 @@
+# Overriding Methods
 class Message:
       def __init__(self, sender, recipient, text):
           self.sender = sender
@@ -16,3 +17,29 @@ class User:
 class Admin(User):
   def edit_message(self, message, new_text):
     message.text = new_text
+    
+    
+# super()
+class PotatoSalad:
+      def __init__(self, potatoes, celery, onions):
+            self.potatoes = potatoes
+            self.celery = celery
+            self.onions = onions
+
+
+class SpecialPotatoSalad(PotatoSalad):
+  def __init__(self, potatoes, celery, onions):
+    super().__init__(potatoes, celery, onions)
+    self.raisins = 40
+# ingredient = PotatoSalad("potato", "salad", "carrot")
+# print(dir(ingredient))
+# print(ingredient.potatoes)
+# print(ingredient.celery)
+# print(ingredient.onions)
+
+ingredient1 = SpecialPotatoSalad("potato", "salad", "carrot")
+print(dir(ingredient1))
+print(ingredient1.potatoes)
+print(ingredient1.celery)
+print(ingredient1.onions)
+print(ingredient1.raisins)
